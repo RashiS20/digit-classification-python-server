195d57bc5b45e52cbf5f1e4d9ecaa78f42d075d3
Rashi Singh <rashi.sngh2012@gmail.com>
2026-03-19 20:43:55 +0530

    small fix

--- app.py
+++ app.py
@@ -39,6 +39,9 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
-    app.run(host="0.0.0.0", port=port)
+    print("Starting app...")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
